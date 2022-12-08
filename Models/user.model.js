469e03cb4c274b4319d4dffc4bbf8a7fd6be6cf9
Rasmus Jensen <rasmusdj96@gmail.com>
2022-12-08 08:50:20 +0100

    Added hash hooks

--- Models/user.model.js
+++ Models/user.model.js
@@ -38,7 +38,14 @@ UserModel.init(
 		modelName: "user",
 		freezeTableName: true,
 		underscored: true,
-		hooks: {},
+		hooks: {
+			beforeCreate: async (user, options) => {
+				user.password = await createHash(user.password);
+			},
+			beforeUpdate: async (user, options) => {
+				user.password = await createHash(user.password);
+			},
+		},
 		//createdAt: false,
 		//updatedAt: false
 	}
